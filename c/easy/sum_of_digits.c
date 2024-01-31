73794ad4e6438c4ae277245679e5195d5fdfda7e
ltfide <lutfidendians@gmail.com>
2024-01-31 21:48:09 +0700

    sum of digits

--- c/easy/sum_of_digits.c
+++ c/easy/sum_of_digits.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+/**
+ * Sum of digits
+ * Example:
+ *  10567 => 16
+ *  1+0+5+6+7 => 16
+ *
+ * Difficulty: Easy
+ * URL: https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number
+ * Date: 31-01-2024
+ * @author Lutfi
+ */
 int main()
 {
     int a = 10564;
+    int sum = 0;
 
-    int i = 0;
-    int x = 1;
-    while (i++ < 6)
+    while (a > 0)
     {
-        printf("%d\n", (a / x) % 10);
-        x = x * 10;
+        sum += a % 10;
+        a /= 10;
     }
+
+    printf("%d", sum); // 16
+    return 0;
 }
