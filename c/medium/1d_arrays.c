5cfdcba358e02bc843c84c9a9f7c0ceed4d895db
ltfide <lutfidendians@gmail.com>
2024-02-09 14:36:04 +0700

    one dimensional array

---
+++ c/medium/1d_arrays.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * One Dimensional Array
+ * Dflty  : Easy
+ * URL    : https://www.hackerrank.com/challenges/1d-arrays-in-c
+ * Date   : 09-02-2024
+ * Author : Lutfi
+*/
+int main() 
+{
+    int n = 6, sum = 0;
+    int ns[6] = {16,13,7,2,1,12};
+
+    int *nums = (int*) malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        nums[i] = ns[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        sum += nums[i];
+    }
+
+    printf("%d", sum);    
+    free(nums);
+    return 0;
+}
